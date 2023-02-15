328ee5d986cd585db2bbde7ce2d9e85efbbe2c4a
bcloutier412 <>
2023-02-15 17:36:21 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Header/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * TOP
+ */
+#include <iostream>
+#include <string>
+
+/*
+ * Functions
+ */
+
+/*
+ * Main function
+ */
+int main(int argc, char* argv[]) {
+  std::string fav_color{"rhino"};
+  
+
+  return 0;
+}
