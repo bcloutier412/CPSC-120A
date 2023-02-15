597f97cfef89b8b129afe4bef497a5f75659824f
Dino <85542673+bcloutier412@users.noreply.github.com>
2023-02-15 18:03:57 +0000

    Used the string library, and implemented a function

---
+++ 02-15 Source Code Setup/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * Headaers
+ */
+#include <iostream>
+#include <string>
+
+/*
+ * Functions
+ */
+// <return type> <function name> <parameters>
+std::string prompt_for(std::string prompt) {
+  std::cout << prompt << '\n';
+  std::string answer{};
+  std::cin >> answer;
+  return answer;
+}
+
+/*
+ * Main
+ */
+int main(int argc, char* argv[]) {
+  std::string fav_color{"cat"};
+  fav_color = prompt_for("what is your favorite color? ");
+  std::cout << fav_color << '\n';
+  return 0;
+}
